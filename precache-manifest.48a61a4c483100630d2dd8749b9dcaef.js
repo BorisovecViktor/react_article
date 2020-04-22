f35c1a50a4737a54cece373752beb19371ee3cd4
BorisovecViktor <viktorborisovets@gmail.com>
2020-04-22 18:10:36 +0300

    Updates

---
+++ precache-manifest.48a61a4c483100630d2dd8749b9dcaef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc8099aac9d9834aec26766c4cfb630",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26d1661a212078421fa6",
+    "url": "./static/js/2.b2110f2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b2110f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1955059e94fc356e85ee",
+    "url": "./static/js/main.fcb111cd.chunk.js"
+  },
+  {
+    "revision": "9a40466c9e3fec559fa3",
+    "url": "./static/js/runtime-main.6a591e0b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.48a61a4c483100630d2dd8749b9dcaef.js"
 );
 
 self.addEventListener('message', (event) => {
